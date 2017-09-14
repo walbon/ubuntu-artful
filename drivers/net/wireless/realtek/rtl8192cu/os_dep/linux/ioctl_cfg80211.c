9c341de01c0ea85c93ad9f5e7f8b2adbc758140d
Stefan Bader <stefan.bader@canonical.com>
2017-09-14 09:48:15 +0200

    UBUNTU: SAUCE: Fix net_device destructor rename
    Was renamed in 4.12 from (struct net_device *)->destructor to
    priv_destructor. Same change was added to stable in 4.11.9.
    
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- drivers/net/wireless/realtek/rtl8192cu/os_dep/linux/ioctl_cfg80211.c
+++ drivers/net/wireless/realtek/rtl8192cu/os_dep/linux/ioctl_cfg80211.c
@@ -3461,7 +3461,11 @@ static int rtw_cfg80211_add_monitor_if(_adapter *padapter, char *name, struct ne
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,1,0))
 	mon_ndev->name_assign_type = name_assign_type;
 #endif
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4,11,9))
 	mon_ndev->destructor = rtw_ndev_destructor;
+#else
+	mon_ndev->priv_destructor = rtw_ndev_destructor;
+#endif
 
 #if (LINUX_VERSION_CODE>=KERNEL_VERSION(2,6,29))
 	mon_ndev->netdev_ops = &rtw_cfg80211_monitor_if_ops;
